b7588c48345da0548345694f83a14c4ec3c7c36e
ManojiConnect <manoj@iconnectsolutions.com>
2025-04-08 13:14:32 +0530

    fix: Remove connection string setting from deployment script to preserve production settings

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -5,13 +5,6 @@ RESOURCE_GROUP="iTrackerResourceGroup"
 APP_SERVICE_NAME="iTrackerApp"
 LOCATION="eastus"
 
-# Set the connection string for SQL Azure database
-az webapp config connection-string set \
-  --resource-group $RESOURCE_GROUP \
-  --name $APP_SERVICE_NAME \
-  --settings DefaultConnection="Server=tcp:itrackersql.database.windows.net,1433;Initial Catalog=iTrackerSQLServer;Persist Security Info=False;User ID=iTrackerDBAdmin;Password=iTracker@2025;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" \
-  --connection-string-type SQLAzure
-
 # Create deployment package
 cd publish && zip -r ../publish.zip . && cd ..
 
